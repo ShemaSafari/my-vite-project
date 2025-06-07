d5632f890647037ce7ce0d80e64320a6b3e83294
Shema Bruno <shemabruno321@gmail.com>
2025-06-07 13:21:46 +0200

    cleaned up some stuff

---
+++ src/assets/slides.js
@@ -0,0 +1,28 @@
+export const slides = [
+    {
+        image: `/Flags/Flag_of_Mozambique.svg.png`,
+        title: 'Mozambique',
+        path: '/Mozambique',
+    },
+    {
+        image: `/Flags/SA flag.png`,
+        title: 'South Africa',
+        path: '/South_Africa',
+    },
+    {
+        image: `/Flags/Flag_of_Malawi.svg.png`,
+        title: 'Malawi',
+        path: '/Malawi',
+    },
+    {
+        image: `/Flags/namibia.png`,
+        title: 'Namibia',
+        path: '/Namibia',
+    },
+    {
+        image: `/Flags/Rwanda flag.png`,
+        title: 'Rwanda',
+        path: '/Rwanda',
+    },
+
+]
--- src/components/Flagslider.jsx
+++ src/components/Flagslider.jsx
@@ -1,6 +1,7 @@
 
 import React,{ useEffect, useState } from "react";
 import Navbar from "./Navbar/Navbar";
+import { slides } from "../assets/slides";
 
 const Flagslider = () => {
 
@@ -29,32 +30,32 @@ const Flagslider = () => {
 
   },[])
 
-  const slides = [
-    {
-        image: `/Flags/Flag_of_Mozambique.svg.png`,
-        title: 'Mozambique',
-        path: '/Mozambique',
-    },
-    {
-        image: `/Flags/SA flag.png`,
-        title: 'South Africa',
-        path: '/South Africa',
-    },
-    {
-        image: `/Flags/Flag_of_Malawi.svg.png`,
-        title: 'Malawi',
-        path: '/Malawi',
-    },
-    {
-        image: `/Flags/namibia.png`,
-        title: 'Namibia',
-        path: '/Namibia',
-    },
-    {
-        image: `/Flags/Rwanda flag.png`,
-        title: 'Rwanda',
-        path: '/Rwanda',
-    },
+  // const slides = [
+  //   {
+  //       image: `/Flags/Flag_of_Mozambique.svg.png`,
+  //       title: 'Mozambique',
+  //       path: '/Mozambique',
+  //   },
+  //   {
+  //       image: `/Flags/SA flag.png`,
+  //       title: 'South Africa',
+  //       path: '/South Africa',
+  //   },
+  //   {
+  //       image: `/Flags/Flag_of_Malawi.svg.png`,
+  //       title: 'Malawi',
+  //       path: '/Malawi',
+  //   },
+  //   {
+  //       image: `/Flags/namibia.png`,
+  //       title: 'Namibia',
+  //       path: '/Namibia',
+  //   },
+  //   {
+  //       image: `/Flags/Rwanda flag.png`,
+  //       title: 'Rwanda',
+  //       path: '/Rwanda',
+  //   },
     
     // {
     //     image: `/Flags/Flag_of_Nigeria.svg.png`,
@@ -66,10 +67,7 @@ const Flagslider = () => {
     //     title: 'Zimbambwe',
     //     path: '/Zimbabwe',
     // },
-    
-    
-
-]
+// ]
   return (
     <div className="bg-black    h-screen " style={{backgroundImage: `url(${'/ChatGPT-Image-africa.png'}`}}>
       <Navbar/>
